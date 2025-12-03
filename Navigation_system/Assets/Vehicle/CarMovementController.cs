4d811bce73e47a91ab8c4b67c05e60ef8c8c9d71
WarLockeR31 <roman.borisenko2016@gmail.com>
2025-12-03 23:50:24 +0300

    feat: add ManualInputDriver and CarMovementController logic

--- Navigation_system/Assets/Vehicle/CarMovementController.cs
+++ Navigation_system/Assets/Vehicle/CarMovementController.cs
@@ -2,18 +2,56 @@ using UnityEngine;
 
 namespace Vehicle
 {
-	public class CarMovementController : MonoBehaviour
-	{
-		// Start is called once before the first execution of Update after the MonoBehaviour is created
-		void Start()
-		{
+    [RequireComponent(typeof(Rigidbody))] 
+    public class CarMovementController : MonoBehaviour
+    {
+        [Header("Settings")]
+        [Tooltip("Units/sec")]
+        [SerializeField] private float maxSpeed = 10f;
         
-		}
+        [Tooltip("Degree/sec")]
+        [SerializeField] private float turnSpeed = 100f;
+        
+        [Tooltip("Units/sec^2")]
+        [SerializeField] private float acceleration = 5f;
 
-		// Update is called once per frame
-		void Update()
-		{
+        private float _currentSpeed = 0f;
+
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+		
+        /// <param name="throttleInput">Вход газа: от -1 (назад) до 1 (вперед).</param>
+        /// <param name="steeringInput">Вход руля: от -1 (влево) до 1 (вправо).</param>
+        public void Move(float throttleInput, float steeringInput)
+        {
+            throttleInput = Mathf.Clamp(throttleInput, -1f, 1f);
+            steeringInput = Mathf.Clamp(steeringInput, -1f, 1f);
+
+            // Velocity calculation
+            float targetSpeed = throttleInput * maxSpeed;
+            _currentSpeed = Mathf.MoveTowards(_currentSpeed, targetSpeed, acceleration * Time.deltaTime);
+
+            // Moving
+            Vector3 translation = Vector3.forward * _currentSpeed * Time.deltaTime;
+            transform.Translate(translation);
+
+            // Steering
+            if (Mathf.Abs(_currentSpeed) > 0.1f)
+            {
+				// TODO: ??? Invert steering when moving backwards?
+				float speedFactor = _currentSpeed / maxSpeed;
+                float turnAmount = steeringInput * turnSpeed * speedFactor * Time.deltaTime;
+                transform.Rotate(0, turnAmount, 0);
+            }
+        }
         
-		}
-	}
+        public void HardStop()
+        {
+            _currentSpeed = 0;
+        }
+    }
 }
--- Navigation_system/Assets/Vehicle/Control/ManualInputDriver.cs
+++ Navigation_system/Assets/Vehicle/Control/ManualInputDriver.cs
@@ -1,19 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Vehicle.Control
 {
+	/// <summary>
+	/// Only for debug purposes
+	/// </summary>
+	[RequireComponent(typeof(CarMovementController))]
 	public class ManualInputDriver : MonoBehaviour
 	{
-		// Start is called once before the first execution of Update after the MonoBehaviour is created
-		void Start()
+		private CarMovementController _carController;
+
+		[Header("Input Setup")]
+		[SerializeField] private InputActionReference moveInputRef;
+
+		private void Awake()
 		{
-        
+			_carController = GetComponent<CarMovementController>();
 		}
 
-		// Update is called once per frame
-		void Update()
+		private void OnEnable()
 		{
-        
+			if (moveInputRef != null && moveInputRef.action != null)
+			{
+				moveInputRef.action.Enable();
+			}
+		}
+
+		private void OnDisable()
+		{
+			if (moveInputRef != null && moveInputRef.action != null)
+			{
+				moveInputRef.action.Disable();
+			}
+		}
+
+		private void Update()
+		{
+			if (moveInputRef == null) 
+				return;
+
+			Vector2 inputVector = moveInputRef.action.ReadValue<Vector2>();
+			_carController.Move(inputVector.y, inputVector.x);
 		}
 	}
 }
